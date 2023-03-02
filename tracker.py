5bdb8c5fab74c735dbb0d42d0cfed4480d014248
Pablo Merchan-Rivera <pablo.merchan-rivera@baywa-re.com>
2023-03-02 14:40:06 +0100

    add input option to describe projects and tasks

--- tracker.py
+++ tracker.py
@@ -3,29 +3,39 @@ import datetime
 # Create an empty dictionary to store project working hours
 project_hours = {}
 
-# Function to start working on a project
-def start_project(project_name):
+# Function to start working on a project and task
+def start_work():
+    project_name = input("Enter project name: ")
+    task_name = input("Enter task name: ")
     now = datetime.datetime.now()
-    project_hours[project_name] = {"start_time": now, "end_time": None}
-    print(f"Started working on {project_name} at {now}")
-
-# Function to end working on a project
-def end_project(project_name):
-    now = datetime.datetime.now()
-    if project_name in project_hours:
-        project_hours[project_name]["end_time"] = now
-        elapsed_time = now - project_hours[project_name]["start_time"]
-        print(f"Ended working on {project_name} at {now}. Elapsed time: {elapsed_time}")
+    if project_name not in project_hours:
+        project_hours[project_name] = {}
+    if task_name not in project_hours[project_name]:
+        project_hours[project_name][task_name] = {"start_time": now, "end_time": None}
     else:
-        print(f"You haven't started working on {project_name} yet.")
+        print(f"You're already working on {task_name} in {project_name}.")
+        return
+    print(f"Started working on {task_name} in {project_name} at {now}")
+
+# Function to end working on a project and task
+def end_work():
+    project_name = input("Enter project name: ")
+    task_name = input("Enter task name: ")
+    now = datetime.datetime.now()
+    if project_name in project_hours and task_name in project_hours[project_name]:
+        project_hours[project_name][task_name]["end_time"] = now
+        elapsed_time = now - project_hours[project_name][task_name]["start_time"]
+        print(f"Ended working on {task_name} in {project_name} at {now}. Elapsed time: {elapsed_time}")
+    else:
+        print(f"You haven't started working on {task_name} in {project_name} yet.")
 
 # Example usage
-start_project("Project A")
-end_project("Project A")
-start_project("Project B")
-end_project("Project B")
+start_work()
+end_work()
 
-# Print the working hours for each project
-for project_name, project_time in project_hours.items():
-    elapsed_time = project_time["end_time"] - project_time["start_time"]
-    print(f"{project_name}: {elapsed_time}")
+# Print the working hours for each project and task
+for project_name, project_tasks in project_hours.items():
+    print(f"Project: {project_name}")
+    for task_name, task_time in project_tasks.items():
+        elapsed_time = task_time["end_time"] - task_time["start_time"] if task_time["end_time"] is not None else datetime.timedelta(0)
+        print(f" - Task: {task_name}, Elapsed time: {elapsed_time}")
